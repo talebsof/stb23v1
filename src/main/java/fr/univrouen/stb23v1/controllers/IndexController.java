52e0d1ca8efc7d9376782471b2e0332c59e82e1f
talebsof <talebsofiane8@gmail.com>
2023-04-05 16:40:28 +0200

    correction vite fait

--- src/main/java/fr/univrouen/stb23v1/controllers/IndexController.java
+++ src/main/java/fr/univrouen/stb23v1/controllers/IndexController.java
@@ -1,5 +1,16 @@
 package fr.univrouen.stb23v1.controllers;
 
-public class IndexController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+public class IndexController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "Hello stb23 !";
+	}
+	
+	
 }
